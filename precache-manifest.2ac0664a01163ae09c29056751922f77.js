e9079e2f5b58caf5a1e8dfcc01bbb2e656a56a59
s-m-9 <maga0030@algonquinlive.com>
2018-12-04 13:38:33 -0500

    Updates

---
+++ precache-manifest.2ac0664a01163ae09c29056751922f77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e59a7342451f7910e62d3458cdcee9d",
+    "url": "/redux-user-react/static/media/user.1e59a734.png"
+  },
+  {
+    "revision": "bdee471c639974f5cc4f88c2031eb935",
+    "url": "/redux-user-react/static/media/time.bdee471c.svg"
+  },
+  {
+    "revision": "5a0fca63e5ca8afe3412",
+    "url": "/redux-user-react/static/js/runtime~main.5a0fca63.js"
+  },
+  {
+    "revision": "8827752064d80c7840bd",
+    "url": "/redux-user-react/static/js/main.88277520.chunk.js"
+  },
+  {
+    "revision": "c43cdadc25a0441b0a65",
+    "url": "/redux-user-react/static/js/1.c43cdadc.chunk.js"
+  },
+  {
+    "revision": "8827752064d80c7840bd",
+    "url": "/redux-user-react/static/css/main.ea2cc796.chunk.css"
+  },
+  {
+    "revision": "c43cdadc25a0441b0a65",
+    "url": "/redux-user-react/static/css/1.d22959ec.chunk.css"
+  },
+  {
+    "revision": "5270407d2fe95aa47a5ddcd7ffa8cdde",
+    "url": "/redux-user-react/index.html"
+  }
+];
